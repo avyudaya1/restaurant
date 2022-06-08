22d3101b7a885c28c0ff9d90558d402590daf15b
avyudaya1 <avyudaya1@gmail.com>
2022-06-08 14:46:21 +0545

    added all models according to er diagram: need some tweaks

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from main.models import Item
+from main.models import Item, ItemCategory, MenuItem, MenuItemCategory, Store
 
 # Register your models here.
 admin.site.register(Item)
+admin.site.register(ItemCategory)
+admin.site.register(MenuItem)
+admin.site.register(MenuItemCategory)
+admin.site.register(Store)
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,7 +1,60 @@
+from pyexpat import model
+from tabnanny import verbose
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.forms import ValidationError
+
+class ItemCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Item Categories'
+    
+    def __str__(self):
+        return self.name
 
 class Item(models.Model):
+    name = models.CharField(max_length=255)
+    unit_choices = ([0, 'Kilogram'], [1, 'Litre'],)
+    unit = models.IntegerField(choices=unit_choices)
+    quantity = models.FloatField(validators=[MinValueValidator(0.0)])
+    manufacture_date = models.DateField()
+    expiry_date = models.DateField()
+    item_category = models.ForeignKey(ItemCategory, blank=False, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItemCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Menu item Categories'
+    
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    ingredients = models.ManyToManyField(Item)
+    cooking_time = models.TimeField()
+    cost = models.FloatField(validators=[MinValueValidator(0.0)])
+    menu_category = models.ForeignKey(MenuItemCategory, blank=False, on_delete=models.SET_NULL, null=True)
+
+    
+    def __str__(self):
+        return self.name
+
+class Store(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = 'Store'
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Store.objects.exists():
+            raise ValidationError('There is can be only one Store instance')
+        return super(Store, self).save(*args, **kwargs)
